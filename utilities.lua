a76197c976a42f99e5f80bb6bdd0fca0fb01d8ba
RDMRDM1 <rdmgamer1234@gmail.com>
2025-09-05 23:08:56 -0400

    Update utilities.lua

--- utilities.lua
+++ utilities.lua
@@ -1,73 +1,80 @@
+-- =========================
+-- Prevent multiple injections
+-- =========================
+if _G.MyMenuInjected then
+    return
+end
+_G.MyMenuInjected = true
+
+-- =========================
+-- Menu Settings
+-- =========================
 local MenuSize = vec2(600, 350)
-local MenuStartCoords = vec2(500, 500) 
+local MenuStartCoords = vec2(500, 500)
 
-local TabsBarWidth = 0 -- The width of the tabs bar, height is assumed to be MenuHeight as it goes top to bottom
+local TabsBarWidth = 0
+local SectionChildWidth = MenuSize.x - TabsBarWidth
+local SectionsCount = 3
+local SectionsPadding = 10
+local MachoPaneGap = 10
 
-local SectionChildWidth = MenuSize.x - TabsBarWidth -- The total size for sections on the left hand side
-local SectionsCount = 3 
-local SectionsPadding = 10 -- pixels between each section (that makes SetionCount + 1 = total padding areas)
-local MachoPaneGap = 10 -- Hard coded gap of accent at the top.
-
--- Therefore each section width must be:
 local EachSectionWidth = (SectionChildWidth - (SectionsPadding * (SectionsCount + 1))) / SectionsCount
 
-
--- Now you have each sections absolute width, you can calculate their X coordinate and Y coordinate
 local SectionOneStart = vec2(TabsBarWidth + (SectionsPadding * 1) + (EachSectionWidth * 0), SectionsPadding + MachoPaneGap)
-local SectionOneEnd = vec2(SectionOneStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+local SectionOneEnd   = vec2(SectionOneStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
 
 local SectionTwoStart = vec2(TabsBarWidth + (SectionsPadding * 2) + (EachSectionWidth * 1), SectionsPadding + MachoPaneGap)
-local SectionTwoEnd = vec2(SectionTwoStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+local SectionTwoEnd   = vec2(SectionTwoStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
 
 local SectionThreeStart = vec2(TabsBarWidth + (SectionsPadding * 3) + (EachSectionWidth * 2), SectionsPadding + MachoPaneGap)
-local SectionThreeEnd = vec2(SectionThreeStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+local SectionThreeEnd   = vec2(SectionThreeStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
 
--- Create our window, MenuStartCoords is where the menu starts
-MenuWindow = MachoMenuWindow(MenuStartCoords.x, MenuStartCoords.y, MenuSize.x, MenuSize.y)
-
+-- =========================
+-- Create Menu Window (starts hidden)
+-- =========================
+local MenuWindow = MachoMenuWindow(MenuStartCoords.x, MenuStartCoords.y, MenuSize.x, MenuSize.y)
 MachoMenuSetAccent(MenuWindow, 137, 52, 235)
+MachoMenuSetVisible(MenuWindow, false) -- Start hidden
 
-
--- First tab
-FirstSection = MachoMenuGroup(MenuWindow, "Section One", SectionOneStart.x, SectionOneStart.y, SectionOneEnd.x, SectionOneEnd.y)
-
+-- =========================
+-- Section One
+-- =========================
+local FirstSection = MachoMenuGroup(MenuWindow, "Section One", SectionOneStart.x, SectionOneStart.y, SectionOneEnd.x, SectionOneEnd.y)
 MachoMenuButton(FirstSection, "Close", function()
-    MachoMenuDestroy(MenuWindow)
-  end)
+    MachoMenuSetVisible(MenuWindow, false)
+end)
 
--- Second tab
-SecondSection = MachoMenuGroup(MenuWindow, "Section Two", SectionTwoStart.x, SectionTwoStart.y, SectionTwoEnd.x, SectionTwoEnd.y)
+-- =========================
+-- Section Two
+-- =========================
+local SecondSection = MachoMenuGroup(MenuWindow, "Section Two", SectionTwoStart.x, SectionTwoStart.y, SectionTwoEnd.x, SectionTwoEnd.y)
 
-MenuSliderHandle = MachoMenuSlider(SecondSection, "Slider", 10, 0, 100, "%", 0, function(Value)
+local MenuSliderHandle = MachoMenuSlider(SecondSection, "Slider", 10, 0, 100, "%", 0, function(Value)
     print("Slider updated with value ".. Value)
 end)
 
 MachoMenuCheckbox(SecondSection, "Checkbox", 
-    function()
-        print("Enabled")
-    end,
-    function()
-        print("Disabled")
-    end
+    function() print("Enabled") end,
+    function() print("Disabled") end
 )
 
-TextHandle = MachoMenuText(SecondSection, "SomeText")
-
+local TextHandle = MachoMenuText(SecondSection, "SomeText")
 MachoMenuButton(SecondSection, "Change Text Example", function()
     MachoMenuSetText(TextHandle, "ChangedText")
-  end)
+end)
 
+-- =========================
+-- Section Three
+-- =========================
+local ThirdSection = MachoMenuGroup(MenuWindow, "Section Three", SectionThreeStart.x, SectionThreeStart.y, SectionThreeEnd.x, SectionThreeEnd.y)
 
--- Third tab
-ThirdSection = MachoMenuGroup(MenuWindow, "Section Three", SectionThreeStart.x, SectionThreeStart.y, SectionThreeEnd.x, SectionThreeEnd.y)
-
-InputBoxHandle = MachoMenuInputbox(ThirdSection, "Input", "...")
+local InputBoxHandle = MachoMenuInputbox(ThirdSection, "Input", "...")
 MachoMenuButton(ThirdSection, "Print Input", function()
     local LocatedText = MachoMenuGetInputbox(InputBoxHandle)
     print(LocatedText)
-  end)
+end)
 
-DropDownHandle = MachoMenuDropDown(ThirdSection, "Drop Down", 
+local DropDownHandle = MachoMenuDropDown(ThirdSection, "Drop Down", 
     function(Index)
         print("New Value is " .. Index)
     end, 
@@ -75,3 +82,19 @@ DropDownHandle = MachoMenuDropDown(ThirdSection, "Drop Down",
     "Selectable 2",
     "Selectable 3"
 )
+
+-- =========================
+-- Menu Toggle Thread
+-- =========================
+local MENU_TOGGLE_KEY = 137 -- CAPS LOCK (change to desired key if needed)
+local menuOpen = false
+
+CreateThread(function()
+    while true do
+        Wait(0)
+        if IsControlJustPressed(0, MENU_TOGGLE_KEY) then
+            menuOpen = not menuOpen
+            MachoMenuSetVisible(MenuWindow, menuOpen)
+        end
+    end
+end)
