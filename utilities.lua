72202f0cd1b2722a6129ddb3496139ada04e84b6
RDMRDM1 <rdmgamer1234@gmail.com>
2025-09-06 02:18:36 -0400

    Update utilities.lua

--- utilities.lua
+++ utilities.lua
@@ -1,108 +1,49 @@
-local MenuSize = vec2(600, 350)
-local MenuStartCoords = vec2(500, 500) 
+local MenuSize = vec2(800, 500)
+local MenuStartCoords = vec2(200, 200)
 
-local TabsBarWidth = 0 -- The width of the tabs bar, height is assumed to be MenuHeight as it goes top to bottom
+-- Create main window
+local MenuWindow = MachoMenuWindow(MenuStartCoords.x, MenuStartCoords.y, MenuSize.x, MenuSize.y)
+MachoMenuSetAccent(MenuWindow, 200, 50, 50)
+MachoMenuSetVisible(MenuWindow, false) -- start hidden
 
-local SectionChildWidth = MenuSize.x - TabsBarWidth -- The total size for sections on the left hand side
-local SectionsCount = 3 
-local SectionsPadding = 10 -- pixels between each section (that makes SetionCount + 1 = total padding areas)
-local MachoPaneGap = 10 -- Hard coded gap of accent at the top.
+-- Sections (one per tab)
+local Sections = {}
 
--- Therefore each section width must be:
-local EachSectionWidth = (SectionChildWidth - (SectionsPadding * (SectionsCount + 1))) / SectionsCount
+Sections["Self"] = MachoMenuGroup(MenuWindow, "Self", 20, 60, MenuSize.x - 20, MenuSize.y - 20)
+Sections["Server"] = MachoMenuGroup(MenuWindow, "Server", 20, 60, MenuSize.x - 20, MenuSize.y - 20)
 
+-- Start with only Self visible
+for _, sec in pairs(Sections) do MachoMenuSetVisible(sec, false) end
+MachoMenuSetVisible(Sections["Self"], true)
 
--- Now you have each sections absolute width, you can calculate their X coordinate and Y coordinate
-local SectionOneStart = vec2(TabsBarWidth + (SectionsPadding * 1) + (EachSectionWidth * 0), SectionsPadding + MachoPaneGap)
-local SectionOneEnd = vec2(SectionOneStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+-- Tab buttons row
+local function AddTabButton(tabName)
+    MachoMenuButton(MenuWindow, tabName, function()
+        for _, sec in pairs(Sections) do
+            MachoMenuSetVisible(sec, false)
+        end
+        MachoMenuSetVisible(Sections[tabName], true)
+    end)
+end
 
-local SectionTwoStart = vec2(TabsBarWidth + (SectionsPadding * 2) + (EachSectionWidth * 1), SectionsPadding + MachoPaneGap)
-local SectionTwoEnd = vec2(SectionTwoStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+AddTabButton("Self")
+AddTabButton("Server")
 
-local SectionThreeStart = vec2(TabsBarWidth + (SectionsPadding * 3) + (EachSectionWidth * 2), SectionsPadding + MachoPaneGap)
-local SectionThreeEnd = vec2(SectionThreeStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+-- Add buttons to "Self" tab
+MachoMenuButton(Sections["Self"], "Godmode", function() print("Godmode") end)
+MachoMenuButton(Sections["Self"], "Invisibility", function() print("Invisibility") end)
 
--- Create our window, MenuStartCoords is where the menu starts
-MenuWindow = MachoMenuWindow(MenuStartCoords.x, MenuStartCoords.y, MenuSize.x, MenuSize.y)
+-- Add buttons to "Server" tab
+MachoMenuButton(Sections["Server"], "Explode Player", function() print("Explode Player") end)
+MachoMenuButton(Sections["Server"], "Freeze Player", function() print("Freeze Player") end)
 
-MachoMenuSetAccent(MenuWindow, 150, 0, 0)
-
-
--- First tab
--- Original:
--- FirstSection = MachoMenuGroup(MenuWindow, "Util.lua", SectionOneStart.x, SectionOneStart.y, SectionOneEnd.x, SectionOneEnd.y)
-
--- Make section slightly taller for bigger title
-local SectionOneStartModified = vec2(SectionOneStart.x, SectionOneStart.y - 10) -- move start up 10 px
-local SectionOneEndModified = vec2(SectionOneEnd.x, SectionOneEnd.y) -- keep same bottom
-
-FirstSection = MachoMenuGroup(MenuWindow, "Util.lua", SectionOneStartModified.x, SectionOneStartModified.y, SectionOneEndModified.x, SectionOneEndModified.y)
-
-MachoMenuButton(FirstSection, "Close", function()
-    MachoMenuDestroy(MenuWindow)
-  end)
-
-MachoMenuButton(FirstSection, "Self", function()
-        
-  end)
-
-MachoMenuButton(FirstSection, "Spawner", function()
-        
-  end)
-
-MachoMenuButton(FirstSection, "Events", function()
-        
-  end)
-
-MachoMenuButton(FirstSection, "Teleport", function()
-        
-  end)
-
-MachoMenuButton(FirstSection, "Troll", function()
-        
-  end)
-
-MachoMenuButton(FirstSection, "Vehicle", function()
-
-  end)
-
--- Second tab
-SecondSection = MachoMenuGroup(MenuWindow, "Section Two", SectionTwoStart.x, SectionTwoStart.y, SectionTwoEnd.x, SectionTwoEnd.y)
-
-MenuSliderHandle = MachoMenuSlider(SecondSection, "Slider", 10, 0, 100, "%", 0, function(Value)
-    print("Slider updated with value ".. Value)
-end)
-
-MachoMenuCheckbox(SecondSection, "Checkbox", 
-    function()
-        print("Enabled")
-    end,
-    function()
-        print("Disabled")
+-- Toggle menu with Caps Lock (137)
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if IsControlJustPressed(0, 137) then
+            local state = MachoMenuGetVisible(MenuWindow)
+            MachoMenuSetVisible(MenuWindow, not state)
+        end
     end
-)
-
-TextHandle = MachoMenuText(SecondSection, "SomeText")
-
-MachoMenuButton(SecondSection, "Change Text Example", function()
-    MachoMenuSetText(TextHandle, "ChangedText")
-  end)
-
-
--- Third tab
-ThirdSection = MachoMenuGroup(MenuWindow, "Section Three", SectionThreeStart.x, SectionThreeStart.y, SectionThreeEnd.x, SectionThreeEnd.y)
-
-InputBoxHandle = MachoMenuInputbox(ThirdSection, "Input", "...")
-MachoMenuButton(ThirdSection, "Print Input", function()
-    local LocatedText = MachoMenuGetInputbox(InputBoxHandle)
-    print(LocatedText)
-  end)
-
-DropDownHandle = MachoMenuDropDown(ThirdSection, "Drop Down", 
-    function(Index)
-        print("New Value is " .. Index)
-    end, 
-    "Selectable 1",
-    "Selectable 2",
-    "Selectable 3"
-)
+end)
